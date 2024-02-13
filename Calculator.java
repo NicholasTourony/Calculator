39f4d738be760a991730f619d7c5420383d79e48
NicholasTourony <natourony@quinnipiac.edu>
2024-02-12 19:48:54 -0500

    Added the option to subtract rather than add

--- Calculator.java
+++ Calculator.java
@@ -3,11 +3,23 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the first whole number to add together:");
+        System.out.println("Enter the first whole number to calculate together:");
         int firstNum = scanner.nextInt();
-        System.out.println("Enter the second whole number to add together:");
+        scanner.nextLine();
+        System.out.println("Enter the second whole number to calculate together:");
         int secondNum = scanner.nextInt();
-        int sum = firstNum + secondNum;
-        System.out.println("The sum of those two numbers is: " + sum);
+        scanner.nextLine();
+        System.out.println("Would you like to + or - the two numbers: ");
+        String operation = scanner.nextLine();
+        if (operation.equals("+")) {
+            int sum = firstNum + secondNum;
+            System.out.println("The sum of those two numbers is: " + sum);
+        }
+        else if (operation.equals("-")) {
+            int solution = firstNum - secondNum;
+            System.out.println("The solution of those two numbers is: " + solution);
+        }
+        
+        
     }
 }
